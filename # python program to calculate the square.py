802a8370f9fde2429b58aacb5a6aefec8c33a3d8
shubhamsuganolu <142864822+shubhamsuganolu@users.noreply.github.com>
2023-08-22 10:39:26 +0530

    Add files via upload

---
+++ # python program to calculate the square.py
@@ -0,0 +1,10 @@
+# python program to calculate the square root
+
+#Note: change this value for a different result
+num = 8
+
+# to take the input from the user
+#num = float(inpu('Enter number:'))
+
+num_sqrt =num**0.5
+print('The square root of %0.3f is %0.3f'%(num,num,sqrt))
