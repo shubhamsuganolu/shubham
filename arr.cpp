776b7a76832144d30c7f462827a15c1fabc7a78f
shubhamsuganolu <142864822+shubhamsuganolu@users.noreply.github.com>
2023-09-11 15:14:32 +0530

    Add files via upload

--- arr.cpp
+++ arr.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
+ 
 
-int main()
-{
-      int arr[5];
-      arr[0]=5;
-      arr[2]=-10;
-
-      //this is same as arr [1]=2 
-      arr[3/2]=2;
-      arr[3]=arr[0];
-
-      cout<<arr[0]<<""<<arr[1]
-            <<""<<arr[2]<<""<<arr[3];
-       return 0;                                                                                                                                                                                                                                                                           
+int main() {
+    string cars[4]={"Volvo","BMW","Ford","Mazad"};
+    cars[0]="Opel";
+    cout<<cars{0};
+    return 0;
 }
